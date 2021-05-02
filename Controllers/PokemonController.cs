7b0c48b6fc0d71d2a46e2654e5865a9d5c65e416
Tristan Covemaeker <tristan.covemaeker@student.howest.be>
2021-05-02 19:32:26 +0200

    added get call

--- Controllers/PokemonController.cs
+++ Controllers/PokemonController.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PokemonAPI.Models;
+using PokemonAPI.Services;
 
 namespace PokemonAPI.Controllers
 {
     [ApiController]
     public class PokemonController : ControllerBase
     {
+        private IPokemonService _pokemonservice;
+
+        public PokemonController(IPokemonService pokemonService)
+        {
+            _pokemonservice = pokemonService;
+        }
         [HttpGet]
-        [Route("pokemon")]
-        public ActionResult<string> GetPokemon(){
-            return new OkObjectResult("Test geslaagd");
+        [Route("types")]
+        public async Task<List<PokeType>> GetTypes(){
+            return await _pokemonservice.GetPokeTypes();
         }
     }
 }
